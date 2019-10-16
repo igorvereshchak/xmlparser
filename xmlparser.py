269545c7cc8d1229f65c3c40c99efd0d5e6cb5b4
Igor Vereshchak <igor.vereshchak@gmail.com>
2019-10-16 18:42:33 +0300

    second day

--- xmlparser.py
+++ xmlparser.py
@@ -7,24 +7,58 @@ from xml.etree.ElementTree import ParseError as er
 import dbf
 
 
+owners = []
+
+def get_field_text(node, field_name, default_value=""):
+    return default_value if node.find(field_name) is None else node.find(field_name).text
+
+def get_address(node):
+
+    fields = ['Street', 'House', 'Aprt', 'City', 'District', 'State', 'PostCode']
+    adr = []
+
+    for field in fields:
+        if node.findtext(field):
+            adr.append(node.findtext(field))
+
+    return ", ".join(adr)
+
+
 #check dublicates
 def parse_xml(fname):
+    global owners
+
     try:
         tree = et.ElementTree(file=fname)
     except er:
         exit('Parsing error')
     root = tree.getroot()
+
+    record_date = root.find('RecordDate').text[:10]
+
     for child in root.findall('./Custodians//CustodianElement//Owners//OwnerElement'):
+        record = {}
         owner = child.find('Owner')
-        Name = None if owner[0].find('Name') is None else owner[0].find('Name').text
-        print('\tOwner type:', owner[0].tag, '\tCountStock: ', child.find('CountStock').text, '\tAccount: ', owner[0].find('Account').text, \
-            '\tName: ', Name)
-        
+        owner_type = owner[0]
+        record['DATE'] = record_date
+        record['KOD'] = 'Ф' if owner_type.tag == 'OwnerIndividual' else 'Ю'
+        name = get_field_text(owner_type, 'Name')
+        citizenship = get_field_text(owner_type, 'Citizenship', '-')
+        record['NAME'] = "{0} ({1})".format(name, citizenship) 
+        record['DEPO_ID'] = get_field_text(owner_type, 'Account')
+        record['R2'] = get_address(owner_type.find('Address'))
+        owners.append(record)
+        #print('\tOwner type:', owner[0].tag, '\tCountStock: ', child.find('CountStock').text, '\tAccount: ', owner[0].find('Account').text, \
+        #    '\tName: ', Name)
+    for o in owners:
+        print(o)
 def conv(txt):
     
     return txt.encode('utf-8').decode('utf-8')
 
 def process_dbf(fname):
+
+    global owners
     
     table = dbf.Table('dbase.dbf', 'DATE c(10); CUST_ID C(6); CUST_NAME c(70); DEPO_ID c(17);\
         KOD c(1); NAME c(254); RL c(254); R2 c(254); NALOG_CODE c(26); BORN_PLACE c(50);\
@@ -33,33 +67,36 @@ def process_dbf(fname):
         SANKCII c(80)', codepage='cp1251')
        
     table.open(mode=dbf.READ_WRITE)
-    rec = {}
-    rec["DATE"] = '13.04.2018'
-    rec["CUST_ID"] = '402842'
-    rec["CUST_NAME"] = 'ТОВ "ОБ\'ЄДНАНА РЕЄСТРАЦІЙНА КОМПАНІЯ"'
-    rec["DEPO_ID"] = '402842-UA10018839'
-    rec["KOD"] = 'Ф'
-    rec["NAME"] = 'Кос Костянтин (Україна)'
-    rec["RL"] = 'Серiя ВА №111111 Видан. 04.11.1995р. Київським РВ УМВС України в Дон. обл.'
-    rec["R2"] = 'пр.Пан, б25, корп.1, кв.16, м.Донецьк, Донецька область, 83001'
-    rec["NALOG_CODE"] = '999999999'
-    rec["BORN_PLACE"] = 'м.Донецьк 07.09.1979'
-    rec["BORN_DATE"] = '08.07.1981'
-    rec["SUM_PAP"] = 1020
-    rec["SUM_BLOCK"] = 1020
-    rec["SUM_QUO"]  = 1
-    rec["OBT"] = "1020(б.р.423000);"
-    rec["SUM_COST"] = 255
-    rec["PERCENT"] = 0.002894
-    rec["BANK"] = ""
-    rec["DIV_KIND"] = 0 
-    rec["SANKCII"] = ""
-    table.append(rec)
+
+    for owner in owners:
+        rec = {}
+        rec["DATE"] = owner['DATE']
+        rec["CUST_ID"] = ''
+        rec["CUST_NAME"] = ''
+        rec["DEPO_ID"] = owner['DEPO_ID']
+        rec["KOD"] = owner['KOD']
+        rec["NAME"] = owner['NAME']
+        rec["RL"] = ''
+        rec["R2"] = owner['R2']
+        rec["NALOG_CODE"] = ''
+        rec["BORN_PLACE"] = ''
+        rec["BORN_DATE"] = ''
+        rec["SUM_PAP"] = 1
+        rec["SUM_BLOCK"] = 0
+        rec["SUM_QUO"]  = 0
+        rec["OBT"] = ""
+        rec["SUM_COST"] = 1
+        rec["PERCENT"] = 0
+        rec["BANK"] = ""
+        rec["DIV_KIND"] = 0 
+        rec["SANKCII"] = ""
+        table.append(rec)
+    print('Dattabse complete!')
     table.close()
 
 def main():
     parse_xml('reestr.xml')
-    #process_dbf('dbase.dbf')
+    process_dbf('dbase.dbf')
 
 if __name__ == "__main__":
     main()
