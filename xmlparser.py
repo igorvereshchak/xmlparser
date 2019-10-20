614c269f4e898adc26927018c01258efb93b1b01
igor <igor.vereshchak@gmail.com>
2019-10-20 20:36:09 +0300

    info update

--- xmlparser.py
+++ xmlparser.py
@@ -11,6 +11,7 @@ import dbf
 
 
 owners = []
+infos = []
 
 def get_field_text(node, field_name, default_value=""):
     return default_value if node.find(field_name) is None else node.find(field_name).text
@@ -30,6 +31,7 @@ def get_address(node):
 #check dublicates
 def parse_xml(fname):
     global owners
+    global infos
 
     try:
         tree = et.ElementTree(file=fname)
@@ -39,6 +41,10 @@ def parse_xml(fname):
 
     dt = datetime.strptime(root.find('RecordDate').text[:10], "%Y-%m-%d")
     record_date = dt.strftime("%d.%m.%Y")
+
+    issue = root.find('Issue')
+    #paper_id = issue.findtext('ISIN')
+    #paper_kind = issue.findtext('Kind') + issue.findtext('Klass')
 
     for childs in root.findall('./Custodians//CustodianElement//OwnerElements//OwnerElement//Owners'):
         for owner_type in childs:
@@ -62,11 +68,11 @@ def conv(txt):
     
     return txt.encode('utf-8').decode('utf-8')
 
-def process_dbf(fname):
+def process_owner(fname):
 
     global owners
     
-    table = dbf.Table('dbase.dbf', 'DATE c(10); CUST_ID C(6); CUST_NAME c(70); DEPO_ID c(17);\
+    table = dbf.Table(fname, 'DATE c(10); CUST_ID C(6); CUST_NAME c(70); DEPO_ID c(17);\
         KOD c(1); NAME c(254); RL c(254); R2 c(254); NALOG_CODE c(26); BORN_PLACE c(50);\
         BORN_DATE c(10); SUM_PAP n(19, 0); SUM_BLOCK n(19, 0); SUM_QUO n(19, 0); \
         OBT c(150); SUM_COST n(19, 2); PERCENT n(12, 6); BANK c(150); DIV_KIND n(1, 0);\
@@ -100,9 +106,20 @@ def process_dbf(fname):
     print('Database complete!')
     table.close()
 
+def process_info(fname):
+
+    global infos
+
+    table = dbf.Table(fname, 'PAPER_ID c(12); PAER_KIND c(20); REG_NUM c(20); NOMINAL n(15, 2); \
+        CUST_ID c(6); EDRPOU c(26); NAME c(70); ADDRESS c(120); LICENSE c(46); PHONE c(40)', codepage='cp866')
+       
+    table.open(mode=dbf.READ_WRITE)
+    table.close()
+
 def main():
     parse_xml('reestr.xml')
-    process_dbf('dbase.dbf')
+    process_owner('dbase.dbf')
+    process_info('info.dbf')
 
 if __name__ == "__main__":
     main()
